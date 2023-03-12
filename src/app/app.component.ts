1dbee6b51bbfb90d18fb1038c8a8c19bbe23948f
Rafe Nakhuda <nakhudarafe@gmail.com>
2023-03-12 14:38:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SpeedTestService} from 'ng-speed-test';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'speed-test-app';
-}
+  public hasTracked:boolean = false;
+  public isTracking:boolean = false;
+  public iterations:number = 1;
+  public speeds:string[] = [];
+
+  constructor(
+    private speedTestService:SpeedTestService
+  ) {
+
+  }
+  trackSpeed():void {
+    if (this.hasTracked) {
+      this.speeds = [];
+
+      this.hasTracked = false;
+    }
+
+    if (this.iterations > 100) {
+      this.iterations = 100;
+    }
+
+    this.isTracking = true;
+
+    this.speedTestService.getMbps({ iterations: 1, retryDelay: 1500 }).subscribe(
+      (speed) => {
+        this.speeds.unshift(
+          speed.toFixed(2)
+        );
+
+        if (this.speeds.length < this.iterations) {
+          this.trackSpeed();
+        } else {
+          this.isTracking = false;
+          this.hasTracked = true;
+        }
+      }
+    )
+  }
+
+  }
+
